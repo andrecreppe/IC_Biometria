58657da9a3bc53d0d2063ba8baede405fe822657
andrecreppe <andre.creppe@uol.com.br>
2019-03-15 09:40:46 -0300

    Experiment 10 - Initialization
    15-03-19

---
+++ 10_final/z_10.m
@@ -0,0 +1,16 @@
+%Setup
+    clc;
+    close all
+    clear all
+    
+%Quantity
+    num_img = 4;
+    num_pessoas = 20;
+    
+%Load base
+    run load_all;
+    %run lbp;
+    %run compareit;
+    %run plot_hist;
+    %run threshold;
+    %run ROC;
